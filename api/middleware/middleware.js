f50becb6d8865f03c3ff466e4c6c0f0e58131eed
Jazzie91 <jasminecarter88@gmail.com>
2024-09-11 04:14:03 -0500

    w/p

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,33 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+const timeStamp = new Date().toLocaleString()
+const method = req.method
+const url = req.originalUrl
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
